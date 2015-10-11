ff5586337e49f1829a4386ddda5c22881aee2e64
matan <dev.matan@gmail.com>
2015-10-11 08:04:19 +0300

    proper command run and aggregation module

--- build.sbt
+++ build.sbt
@@ -4,28 +4,7 @@ name := "sbt-plugin"
 
 sbtPlugin := true
 
-scalacOptions := Seq("-unchecked", "-deprecation", "-feature", "-encoding", "utf8")
-
-libraryDependencies ++= Seq("canve" %% "compiler-plugin" % "0.0.1")
-
-resolvers ++= {
-  if(isSnapshot.value) Seq(Resolver.mavenLocal, Resolver.sonatypeRepo("snapshots"))
-  else Seq.empty
-}
-
-publishMavenStyle := false
-publishArtifact in Test := false
-
-licenses +=("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0.html"))
-
-ScriptedPlugin.scriptedSettings
-
-scriptedLaunchOpts ++= Seq(
-  "-Xmx1024M", "-XX:MaxPermSize=256M",
-  "-Dplugin.version=" + version.value
-)
-
-releaseSettings
-
-bintrayOrganization := None
-bintrayRepository := "sbt-plugins"
+libraryDependencies ++= Seq("com.github.tototoshi" %% "scala-csv" % "1.3.0-SNAPSHOT",
+                            "canve" %% "compiler-plugin" % "0.0.1",
+                            "canve" %% "simplest" % "0.1-SNAPSHOT")
+                            
--- src/main/scala/canve/sbt/sbtPlugin.scala
+++ src/main/scala/canve/sbt/sbtPlugin.scala
@@ -3,10 +3,32 @@ package canve.sbt
 import sbt.Keys._
 import sbt._
 
+import org.canve.compilerPlugin.Normalize
+//import org.canve.simplest.Foo
+
+import java.io.File
+import com.github.tototoshi.csv._
+
 // TODO: add cleanup as per http://www.scala-sbt.org/0.13.5/docs/Getting-Started/More-About-Settings.html#appending-with-dependencies-and
 
-object ExtractorSbtPlugin extends AutoPlugin {
+object Plugin extends AutoPlugin {
+  
+  override def requires = plugins.JvmPlugin
+  override def trigger = allRequirements
 
+  try { 
+    CSVReader.open(new File("whatever")).allWithHeaders
+  } 
+    catch {
+      case t: Throwable =>
+        println(t)
+    }
+    finally {
+      println("sbt plugin loaded")
+    }
+    
+  //val a = new Foo
+  
   val compilerPluginOrg = "canve"
   val compilerPluginVersion = "0.0.1"
   val compilerPluginArtifact = "compiler-plugin"
@@ -18,9 +40,6 @@ object ExtractorSbtPlugin extends AutoPlugin {
 
   // see: https://github.com/sbt/sbt/issues/1095 or https://github.com/sbt/sbt/issues/780
   val aggregateFilter: ScopeFilter.ScopeFilter = ScopeFilter( inAggregates(ThisProject), inConfigurations(Compile) ) 
-
-  override def requires = plugins.JvmPlugin
-  override def trigger = allRequirements
 
   // global settings needed for the bootstrap
   override lazy val projectSettings = Seq(
@@ -35,12 +54,13 @@ object ExtractorSbtPlugin extends AutoPlugin {
 
   private def instrument(): State => State = { state =>
     val extracted: Extracted = Project.extract(state)
-    val originalSettings = extracted.structure.allProjectRefs
+    val originalSettings = extracted.structure
+    
     val newSettings: Seq[Def.Setting[Task[Seq[String]]]] = extracted.structure.allProjectRefs map { projRef =>
       val projectName = projRef.project
       println("canve instrumenting project " + projectName)
       
-      lazy val ScalacOptions = Def.task {
+      lazy val pluginScalacOptions: Def.Initialize[Task[Seq[String]]] = Def.task {
         // search for the compiler plugin
         val deps: Seq[File] = update.value matching configurationFilter("provided")
         deps.find(_.getAbsolutePath.contains(compilerPluginArtifact)) match {
@@ -53,23 +73,26 @@ object ExtractorSbtPlugin extends AutoPlugin {
             ).flatten
         }
       }
-      scalacOptions in projRef ++= ScalacOptions.value
+      scalacOptions in projRef ++= pluginScalacOptions.value
     }
+    val oldState: Task[Seq[String]] = extracted.get(scalacOptions)
     val newState = extracted.append(newSettings, state)
 
     val structure = extracted.structure
-    
+
     extracted.structure.allProjectRefs map { projRef =>
       println(projRef)
-      EvaluateTask(structure, clean, newState, projRef)
-      EvaluateTask(structure, compile in Test, newState, projRef)
+      EvaluateTask(structure, clean, newState, projRef)           // TODO: if returns None ==> error
+      EvaluateTask(structure, compile in Test, newState, projRef) // TODO: if returns None ==> error
+      val a = new Normalize
+        
     }
 
     println("canve task done")
 
-    newState
+    state
     
   }
   
-  println("sbt canve plugin loaded - use " + sbtLegacyCommandName + " to run canve as part of every compilation")
+  //println("sbt canve plugin loaded - use " + sbtLegacyCommandName + " to run canve as part of every compilation")
 }
